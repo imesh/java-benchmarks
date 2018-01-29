e27caade40393bce1c07e76c2e697ee50e825fc6
Imesh Gunaratne <imesh.gunaratne@gmail.com>
2018-01-29 18:06:55 +0530

    Add FileReaderUtil method comments

--- src/main/java/org/imesh/java/benchmark/file/FileReaderUtil.java
+++ src/main/java/org/imesh/java/benchmark/file/FileReaderUtil.java
@@ -11,11 +11,23 @@ import java.util.stream.Stream;
  */
 public class FileReaderUtil {
 
+    /**
+     * Reade files with java.nio.file.Files
+     * @param filePath
+     * @param callback
+     * @throws IOException
+     */
     public static void readWithFiles(String filePath, LineReaderCallback callback) throws IOException {
         Stream<String> lines = Files.lines(Paths.get(filePath));
         lines.forEach(line -> callback.readLine(line));
     }
 
+    /**
+     * Read files with java.io.BufferedReader
+     * @param filePath
+     * @param callback
+     * @throws IOException
+     */
     public static void readWithBufferedReader(String filePath, LineReaderCallback callback) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(filePath));
         String line;
@@ -24,6 +36,12 @@ public class FileReaderUtil {
         }
     }
 
+    /**
+     * Read files with java.util.Scanner
+     * @param filePath
+     * @param callback
+     * @throws FileNotFoundException
+     */
     public static void readWithScanner(String filePath, LineReaderCallback callback) throws FileNotFoundException {
         Scanner sc = new Scanner(new File(filePath));
         while (sc.hasNextLine()) {
